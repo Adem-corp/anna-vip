b037d22c1ab73ec473132fc268a6ed4bc5ccf8b4
Danil <doink3690@gmail.com>
2026-01-14 17:05:07 +0200

    Оформил блок text

---
+++ layouts/blocks/text/template.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Text template.
+ *
+ * @package Anna-vip
+ * @since 1.0.0
+ */
+
+$text = get_sub_field( 'text' )
+?>
+
+<section class="text">
+	<?php
+	get_template_part(
+		'/layouts/partials/title',
+		null,
+		array(
+			'class' => 'text__title',
+			'title' => get_sub_field( 'title' ),
+		)
+	);
+	?>
+	<?php if ( $text ) : ?>
+		<div class="text__body"><?php echo wp_kses_post( $text ); ?></div>
+	<?php endif; ?>
+</section>
